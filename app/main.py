c29a9ca05166220e25b4b77b19f04dab44271e46
emmyjay387 <104701190+emmyjay387@users.noreply.github.com>
2022-08-23 14:06:09 -0400

    added env variables

--- app/main.py
+++ app/main.py
@@ -9,15 +9,15 @@ models.Base.metadata.create_all(bind=engine)
 #creates an instance of FastApi
 app = FastAPI()
 
-origins = ["*"]
+# origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 #include our post.router
 app.include_router(post.router)
